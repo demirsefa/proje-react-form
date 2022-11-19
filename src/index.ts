3077d8b181b0b799fa7e7bb9deb87554c2792a71
demir <nakitcoins@ga>
2022-11-19 06:27:00 +0300

    version increased logs deleted export / import fixed

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export type {FormRefreshType,InputProps,TextareaProps} from "./models";
+export  {FormRefreshType} from "./models";
+export type {InputProps,TextareaProps} from "./models";
 export {Form,Input,ErrorForGlobal,ErrorForInput,TextArea} from "./dom";
 export {useLoading,useWatch,useInput,useOnSubmit,useErrorForGlobal,useErrorForInput} from "./form-context";
